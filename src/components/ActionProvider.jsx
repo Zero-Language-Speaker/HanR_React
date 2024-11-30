4dc3038f69a6ee17c3526645e5ba4dd9f7bcd009
lectura7942 <>
2024-12-01 04:09:18 +0900

    Fix FastAPI 서버 연결 형식으로 수정

--- src/components/ActionProvider.jsx
+++ src/components/ActionProvider.jsx
@@ -9,10 +9,25 @@ const ActionProvider = ({ createChatBotMessage, setState, children, state}) => {
     console.log(state)
 
     try{
-      const mission = await axios.post('/api/mission', { word: state.word, meaning: state.definition }).then(res => res.data.mission);
+      const mission = await axios.post('http://localhost:8000/api/mission', { 
+        word: state.word, 
+        meaning: state.definition,
+        mission_type: -1,
+        past_missions: [] // not used
+      }).then(res => res.data.mission);
+      console.log("generateMission:", mission)
 
-      const botMessage = createChatBotMessage(`미션: ${mission}`);
-      updateState(botMessage);
+      const botMessage = createChatBotMessage(
+        <div>
+          <h3>미션</h3>
+          <p>{mission}</p>
+        </div>
+      );
+      updateMessages(botMessage);
+      setState((prev) => ({
+        ...prev,
+        mission: mission,
+      }))
 
     } catch (error) {
       console.error("Error generateMission:", error)
@@ -21,20 +36,30 @@ const ActionProvider = ({ createChatBotMessage, setState, children, state}) => {
 
   const generateFeedback = async(message) => {
     try {
-      const feedback = await axios.post('/api/feedback', { word: state.word, meaning: state.definition, user_input: message }).then(res => res.data.message);
+      const feedback = await axios.post('http://localhost:8000/api/feedback', { 
+        word: state.word, 
+        meaning: state.definition, 
+        user_input: message,
+        mission: state.mission,
+        mission_type: -1, // not used
+      }).then(res => res.data.message);
 
       const botMessage = createChatBotMessage(
-        `피드백:\n${feedback}`,{
+        <div>
+          <h3>피드백</h3>
+          <p>{feedback}</p>
+        </div>
+        ,{
           widget: "missionBtn"
         });
-      updateState(botMessage);
+      updateMessages(botMessage);
 
     } catch (error) {
       console.error("Error handleUserInput:", error)
     }
   };
 
-  const updateState = (message) => {
+  const updateMessages = (message) => {
     setState((prev) => ({
       ...prev,
       messages: [...prev.messages, message],
@@ -58,55 +83,5 @@ const ActionProvider = ({ createChatBotMessage, setState, children, state}) => {
     </div>
   );
 };
-
-// class ActionProvider {
-//   constructor(createChatBotMessage, setStateFunc) {
-//     this.createChatBotMessage = createChatBotMessage;
-//     this.setState = setStateFunc;
-//   }
-
-//   generateMission = async ({word, definition}) => {
-//     console.log("Start generateMission")
-//     try {
-//       const loadingMessage = this.createChatBotMessage("Generating your mission...");
-//       this.updateChatbotState(loadingMessage);
-
-//       const response = await axios.post('/api/mission', { word: word, meaning: definition });
-//       // setChatContext({ ...chatContext, "mission": response.data.mission })
-
-//       const missionMessage = this.createChatBotMessage(
-//         `Here's your mission: ${response.data.mission}`,
-//       )
-//       this.updateChatbotState(missionMessage)
-
-//       console.log("generateMission:", response.data)
-//     } catch (err) {
-//       console.error('Error generateMission:', err);
-//       const errorMessage = this.createChatBotMessage(
-//         "Sorry, I couldn't process your request at this time. Please try again later."
-//       );
-//       this.updateChatbotState(errorMessage);
-//     }
-//   };
-
-//   handleLearningMission = (word, mission) => {
-//     const botMessage = this.createChatBotMessage(
-//       `Let's learn about the word "${word}". Here's your mission: ${mission}`
-//     );
-//     this.updateChatbotState(botMessage);
-//   }
-
-//   handleNormalMessage = (message) => {
-//     const botMessage = this.createChatBotMessage(`You said: "${message}". How can I help you with this?`);
-//     this.updateChatbotState(botMessage);
-//   }
-
-//   updateChatbotState = (message) => {
-//     this.setState((prevState) => ({
-//       ...prevState,
-//       messages: [...prevState.messages, message],
-//     }));
-//   }
-// }
 
 export default ActionProvider;
